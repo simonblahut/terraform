0fc9fead7a98781bed5cb2d021b726acd76f934d
Simon Blahut <93943619+simonblahut@users.noreply.github.com>
2023-07-15 12:21:47 +0100

    adding nodered-container

---
+++ nodered-containerised/variables.tf
@@ -0,0 +1,38 @@
+variable "image" {
+  type        = map(any)
+  description = "image for container"
+  default = {
+    dev  = "nodered/node-red:latest"
+    prod = "nodered/node-red:latest-minimal"
+  }
+}
+
+variable "ext_port" {
+  type = map(any)
+
+  validation {
+    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+    error_message = "The external port must be in the valid port range 1980 - 65535."
+  }
+  validation {
+    condition     = max(var.ext_port["prod"]...) < 1980 && min(var.ext_port["prod"]...) >= 1880
+    error_message = "The external port must be in the valid port range 1880 - 1980."
+  }
+}
+
+variable "int_port" {
+  type    = number
+  default = 1880
+
+  validation {
+    condition     = var.int_port == 1880
+    error_message = "The internal port must be 1880."
+  }
+}
+
+locals {
+  resource_count = length(var.ext_port[terraform.workspace])
+}
+
+
+
